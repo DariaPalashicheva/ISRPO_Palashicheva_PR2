fc71ccd08db5aff50ea6efe23cf1b2f122bde851
palashichevaa@mail.ru <palashichevaa@mail.ru>
2022-09-26 10:42:58 +1000

    цифры в текстбокс

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,65 @@ namespace ISRPO_Palashicheva_PR2
         {
             InitializeComponent();
         }
+
+        private void bt01_Click(object sender, RoutedEventArgs e)
+        {
+            tb1.Text = tb1.Text + 0;
+        }
+
+        private void bt1_Click(object sender, RoutedEventArgs e)
+        {
+            tb1.Text = tb1.Text + 1;
+        }
+
+        private void bt2_Click(object sender, RoutedEventArgs e)
+        {
+
+            tb1.Text = tb1.Text + 2;
+        }
+
+        private void bt3_Click(object sender, RoutedEventArgs e)
+        {
+
+            tb1.Text = tb1.Text + 3;
+        }
+
+        private void bt4_Click(object sender, RoutedEventArgs e)
+        {
+
+            tb1.Text = tb1.Text + 4;
+        }
+
+        private void bt5_Click(object sender, RoutedEventArgs e)
+        {
+
+            tb1.Text = tb1.Text + 5;
+        }
+
+        private void bt6_Click(object sender, RoutedEventArgs e)
+        {
+
+            tb1.Text = tb1.Text + 6;
+        }
+
+        private void bt7_Click(object sender, RoutedEventArgs e)
+        {
+
+            tb1.Text = tb1.Text + 7;
+        }
+
+        private void bt8_Click(object sender, RoutedEventArgs e)
+        {
+
+            tb1.Text = tb1.Text + 8;
+        }
+
+        private void bt9_Click(object sender, RoutedEventArgs e)
+        {
+
+            tb1.Text = tb1.Text + 9;
+        }
+
+
     }
 }
